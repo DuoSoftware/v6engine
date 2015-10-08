d88677ab9d37e443f8c44b2a152935890c0b75db
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2015-10-08 14:12:20 +0530

    Update userSVC.go
    new user svc without errors

--- authlib/userSVC.go
+++ authlib/userSVC.go
@@ -1,15 +1,25 @@
 package authlib
 
+import (
+	//"duov6.com/common"
+	"duov6.com/gorest"
+	//"encoding/json"
+	//"fmt"
+)
+
 type userSVC struct {
 	addUserRoles    gorest.EndPoint `method:"POST" path:"/user/AddUserRoles/" postdata:"UserRole"`
 	getMyRoles      gorest.EndPoint `method:"GET" path:"/user/GetMyRoles" output:"[]RoleMinimum"`
 	getRoles        gorest.EndPoint `method:"GET" path:"/user/GetRoles/{GUUserID:string}" output:"[]RoleMinimum"`
 	removeUserRoles gorest.EndPoint `method:"GET" path:"/user/RemoveUserRoles/{RoleID:string}" output:"bool"`
+	addOtherData    gorest.EndPoint `method:"GET" path:"/user/addOtherdata/{UserID:string}/{Filed:string}/{Value:string}" output:"bool"`
+	gorest.RestService
 }
 
 func (A userSVC) AddUserRoles(u UserRole) {
 	h := newAuthHandler()
-	c, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
+	_, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
+
 	if err == "" {
 	} else {
 		A.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("Security Token Not Incorrect"))
@@ -17,9 +27,13 @@ func (A userSVC) AddUserRoles(u UserRole) {
 
 }
 
-func (A userSVC) RemoveUserRoles(string RoleID) bool {
+func (A userSVC) AddOtherData(UserID, Filed, Value string) bool {
+	return true
+}
+
+func (A userSVC) RemoveUserRoles(RoleID string) bool {
 	h := newAuthHandler()
-	c, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
+	_, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
 	if err == "" {
 	} else {
 		A.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("Security Token Not Incorrect"))
@@ -30,7 +44,7 @@ func (A userSVC) RemoveUserRoles(string RoleID) bool {
 
 func (A userSVC) GetMyRoles() []RoleMinimum {
 	h := newAuthHandler()
-	c, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
+	_, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
 	r := []RoleMinimum{}
 	if err == "" {
 		return r
@@ -42,7 +56,7 @@ func (A userSVC) GetMyRoles() []RoleMinimum {
 
 func (A userSVC) GetRoles(UserID string) []RoleMinimum {
 	h := newAuthHandler()
-	c, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
+	_, err := h.GetSession(A.Context.Request().Header.Get("Securitytoken"), "Nil")
 	r := []RoleMinimum{}
 	if err == "" {
 		return r
