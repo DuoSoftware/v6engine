482958cd738898fb4234251d96f86c3ac71f203d
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2015-10-08 14:13:30 +0530

    Update TenantSvc.go
    test

--- authlib/TenantSvc.go
+++ authlib/TenantSvc.go
@@ -18,6 +18,8 @@ type TenantSvc struct {
 	createTenant        gorest.EndPoint `method:"POST" path:"/tenant/CreateTenant/" postdata:"Tenant"`
 	searchTenants       gorest.EndPoint `method:"GET" path:"/tenant/SearchTenants/{SearchString:string}/{pagesize:int}/{startPoint:int}" output:"[]Tenant"`
 	subciribe           gorest.EndPoint `method:"GET" path:"/tenant/Subciribe/{TenantID:string}" output:"bool"`
+	getUsers            gorest.EndPoint `method:"GET" path:"/tenant/GetUsers/{TenantID:string}" output:"[]string"`
+	addUser             gorest.EndPoint `method:"GET" path:"/tenant/AddUser/{email:string}/{level:string}" output:"bool"`
 }
 
 func (T TenantSvc) CreateTenant(t Tenant) {
@@ -35,7 +37,7 @@ func (T TenantSvc) Autherized(TenantID string) (outCrt TenantAutherized) {
 	//fmt.Println(T.Context.Request().Header["SecurityToken"])
 	user, error := session.GetSession(T.Context.Request().Header.Get("Securitytoken"), "Nil")
 	//outCrt = TenantAutherize{}
-
+	//TenantID
 	if error == "" {
 		th := TenantHandler{}
 		b, d := th.Autherized(TenantID, user)
@@ -67,6 +69,22 @@ func (T TenantSvc) GetTenant(TenantID string) Tenant {
 	}
 }
 
+func (T TenantSvc) GetUsers(TenantID string) []string {
+	//fmt.Println(T.Context.Request().Header.Get("Securitytoken"))
+	u, error := session.GetSession(T.Context.Request().Header.Get("Securitytoken"), "Nil")
+	//outCrt = TenantAutherize{}
+	//ul := []User{}
+	if error == "" {
+		th := TenantHandler{}
+		//a := AuthHandler{}
+		//a.GetUser(email)
+		return th.GetUsersForTenant(u, TenantID)
+	} else {
+		T.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("SecurityToken  not Autherized"))
+		return []string{}
+	}
+}
+
 func (T TenantSvc) InviteUser(users []InviteUsers) {
 	//fmt.Println(T.Context.Request().Header.Get("Securitytoken"))
 	user, error := session.GetSession(T.Context.Request().Header.Get("Securitytoken"), "Nil")
@@ -76,6 +94,34 @@ func (T TenantSvc) InviteUser(users []InviteUsers) {
 	} else {
 		T.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("SecurityToken  not Autherized"))
 		return
+	}
+}
+
+func (T TenantSvc) AddUser(email, level string) bool {
+	//fmt.Println(T.Context.Request().Header.Get("Securitytoken"))
+	user, error := session.GetSession(T.Context.Request().Header.Get("Securitytoken"), "Nil")
+	if error == "" {
+		auth := AuthHandler{}
+		a, err := auth.GetUser(email)
+		if err == "" {
+
+			th := TenantHandler{}
+			_, p := th.Autherized(user.Domain, user)
+			if p.SecurityLevel == "admin" {
+				t := th.GetTenant(user.Domain)
+				th.AddUsersToTenant(user.Domain, t.Name, a.UserID, level)
+				return true
+			} else {
+				T.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("Need to have Admin access to tenant to add user"))
+				return false
+			}
+
+		} else {
+			return false
+		}
+	} else {
+		T.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("SecurityToken  not Autherized"))
+		return false
 	}
 }
 
@@ -92,10 +138,8 @@ func (T TenantSvc) AcceptRequest(securityLevel, RequestToken string, accept bool
 }
 
 func (T TenantSvc) GetTenants(securityToken string) []TenantMinimum {
-	//fmt.Println()
 	tns := []TenantMinimum{}
 	user, error := session.GetSession(securityToken, "Nil")
-	//outCrt = TenantAutherize{}
 
 	if error == "" {
 		th := TenantHandler{}
@@ -130,15 +174,12 @@ func (T TenantSvc) GetSampleTenantForm() Tenant {
 }
 
 func (T TenantSvc) Subciribe(TenantID string) bool {
-	//users :=InviteUsers[]{"Sss",sss,sss}
-
-	//fmt.Println(T.Context.Request().Header.Get("Securitytoken"))
 	user, error := session.GetSession(T.Context.Request().Header.Get("Securitytoken"), "Nil")
 	if error == "" {
 		th := TenantHandler{}
 		return th.RequestToTenant(user, TenantID)
 	} else {
 		T.ResponseBuilder().SetResponseCode(401).WriteAndOveride([]byte("SecurityToken  not Autherized"))
-		return
+		return false
 	}
 }
