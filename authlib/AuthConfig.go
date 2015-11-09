a26573d94ec5258d001a6b610295ddcc38db43ee
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2015-11-09 11:33:24 +0530

    Create AuthConfig.go
    AuthConfig

---
+++ authlib/AuthConfig.go
@@ -0,0 +1,13 @@
+package authlib
+
+type AuthConfig struct {
+	Cirtifcate    string
+	PrivateKey    string
+	Https_Enabled bool
+	StoreID       string
+	Smtpserver    string
+	Smtpusername  string
+	Smtppassword  string
+	UserName      string
+	Password      string
+}
