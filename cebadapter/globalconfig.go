ce8a7147628cacf06329979fa8c084e081f80b66
Supun Dissanayake <supuncodes@gmail.com>
2016-02-11 14:19:46 +0530

    global config caching added

--- cebadapter/globalconfig.go
+++ cebadapter/globalconfig.go
@@ -1,6 +1,11 @@
 package cebadapter
 
-import ("duov6.com/ceb")
+import (
+"duov6.com/ceb"
+"duov6.com/config"
+"encoding/json"
+"fmt"
+)
 
 var globalConfigs map[string][]interface{}
 var funcQueue map[string]func(data []interface{})
@@ -11,6 +16,13 @@ func GetLatestGlobalConfig(name string, fn func(data []interface{})) {
 	}
 
 	if globalConfigs[name] == nil {
+
+		if (checkLocalCache(name)){
+			fmt.Println("Store Configuration loaded from LOCAL CACHE!!!!!!!")
+			fn(globalConfigs[name])
+			return
+		}
+
 		if funcQueue == nil {
 			funcQueue = make(map[string]func(data []interface{}))
 		}
@@ -25,6 +37,32 @@ func GetLatestGlobalConfig(name string, fn func(data []interface{})) {
 	}
 }
 
+func checkLocalCache(name string) bool {
+	isLoadedFromCache := false
+
+	bytes,err := config.Get("globalConfig." + name)
+	if err == nil {
+		isLoadedFromCache = true
+		configData := make([]interface{},0)
+		err = json.Unmarshal(bytes, &configData)
+		globalConfigs[name] = configData
+	}
+
+	return isLoadedFromCache
+}
+
+
+func setLocalCache(name string, data []interface{}){
+	dataset, err := json.Marshal(data)
+
+	if err ==nil {
+		plainString := string(dataset[:len(dataset)])
+		config.Save("globalConfig." + name, plainString)
+	}else{
+		fmt.Println(err.Error())
+	}
+}
+
 func GetGlobalConfig(name string) (data []interface{}) {
 
 	data = globalConfigs[name]
@@ -33,6 +71,8 @@ func GetGlobalConfig(name string) (data []interface{}) {
 
 func SetGlobalConfig(key string, value []interface{}) {
 	globalConfigs[key] = value
+
+	setLocalCache(key, value)
 
 	if funcQueue[key] != nil {
 		funcQueue[key](value)
