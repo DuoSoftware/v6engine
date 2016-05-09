26ac3777145737543bb8361b47bfe2acfcc34023
Prasad Jayashanka <prasadjayashanka@gmail.com>
2016-05-09 15:54:31 +0530

    changes

--- objectstore/Transaction/Bucket.go
+++ objectstore/Transaction/Bucket.go
@@ -1,88 +1,64 @@
 package Transaction
 
 import (
-	"duov6.com/common"
+	"duov6.com/objectstore/cache"
 	"duov6.com/objectstore/messaging"
 	"encoding/json"
 	"errors"
-	"github.com/xuyu/goredis"
 	"strings"
 	"time"
 )
 
-func GetConnection(request *messaging.ObjectRequest) (client *goredis.Redis, err error) {
-	client, err = goredis.DialURL("tcp://@" + request.Configuration.ServerConfiguration["REDIS"]["Host"] + ":" + request.Configuration.ServerConfiguration["REDIS"]["Port"] + "/0?timeout=60s&maxidle=60")
-	if err != nil {
-		return nil, err
-	}
-	if client == nil {
-		return nil, errors.New("Connection to REDIS Failed!")
-	}
-	return
-}
+const (
+	Command   = 0
+	Operation = 1
+)
 
-func Create(request *messaging.ObjectRequest) (err error) {
+func NewTransaction(request *messaging.ObjectRequest) (err error) {
 	transactionID := request.Body.Parameters.TransactionID
 	transactionStruct := request.Body.Transaction
 
-	if transactionID == "" && transactionStruct.Type != "" {
-		client, err := GetConnection(request)
+	if transactionID == "" && strings.EqualFold(transactionStruct.Type, "BEGIN") {
 		transactionID = GetTransactionID()
-		bucketName := "Transactions." + request.Controls.Namespace + "." + request.Contorls.Class + "." + transactionID
 
 		metadata := make(map[string]interface{})
 		metadata["TimeStamp"] = time.Now().Format("2006-01-02 15:04:05")
-		metadata["Namespace"] = request.Controls.Namespace
-		metadata["Class"] = request.Controls.Class
 		metadata["TransactionID"] = transactionID
 
 		bucketValue, _ := json.Marshal(metadata)
-		_, err = client.RPush(bucketName, string(bucketValue))
+		err = cache.RPush(request, GetBucketName(transactionID), string(bucketValue))
 	} else {
 		err = errors.New("No Transaction Command Found!")
 	}
 	return
 }
 
-func Add(request *messaging.ObjectRequest) (err error) {
+func AppendTransaction(request *messaging.ObjectRequest) (err error) {
 	transactionID := request.Body.Parameters.TransactionID
 	transactionStruct := request.Body.Transaction
 
 	if transactionID != "" && transactionStruct.Type == "" {
-		bucketName := "Transactions." + request.Controls.Namespace + "." + request.Contorls.Class + "." + transactionID
 		bucketValue, _ := json.Marshal(request)
-		_, err = client.RPush(bucketName, string(bucketValue))
+		err = cache.RPush(request, GetBucketName(transactionID), string(bucketValue))
 	} else {
 		err = errors.New("No TransactionID Found!")
 	}
 	return
 }
 
-func VerifyBlockList(request *messaging.ObjectRequest) (err error) {
-	dbOperation := strings.ToLower(request.Controls.Operation)
-
-	switch dbOperation {
-	case "insert":
-		break
-	case "update":
-		break
-	case "delete":
-		break
-	default:
-		break
-	}
-
-	return err
-}
-
-func AddToBlockList(request *messaging.ObjectRequest) (err error) {
+func CreateBlockEntry(request *messaging.ObjectRequest, TransactionID string) (err error) {
+	//entry := GetBlockEntryName(request, TransactionID)
+	//err = cache.StoreValue(request, entry, request)
 	return
 }
 
-func DeleteFromBlockList(request *messaging.ObjectRequest) (err error) {
+func DeleteBlockEntry(request *messaging.ObjectRequest, TransactionID string) (err error) {
+	//entry := GetBlockEntryName(request)
+	//err = cache.DeleteKey(request, entry)
 	return
 }
 
-func GetTransactionID() string {
-	return common.GetGUID()
+func CheckBlockEntry(request *messaging.ObjectRequest, TransactionID string) (status bool) {
+	//entry := GetBlockEntryName(request, TransactionID)
+	return
 }
