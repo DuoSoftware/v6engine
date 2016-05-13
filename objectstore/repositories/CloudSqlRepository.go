37dae454878d6264cf8386ea64cd32a491a064f3
Prasad Jayashanka <prasadjayashanka@gmail.com>
2016-05-13 10:53:07 +0530

    rr

--- objectstore/repositories/CloudSqlRepository.go
+++ objectstore/repositories/CloudSqlRepository.go
@@ -1379,7 +1379,6 @@ func (repository CloudSqlRepository) sqlToGolang(b []byte, t string) interface{}
 
 	var outData interface{}
 	tmp := string(b)
-	fmt.Println("|" + t + "|")
 	switch t {
 	case "bit(1)":
 		if len(b) == 0 {
