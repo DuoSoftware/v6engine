c10c9d8dcf84d0ca6f15fc48101aa090239582a3
Prasad Jayashanka <prasadjayashanka@gmail.com>
2016-05-16 16:56:22 +0530

    version update

--- objectstore/endpoints/HTTPService.go
+++ objectstore/endpoints/HTTPService.go
@@ -80,7 +80,7 @@ func startKeyFlusher(request *messaging.ObjectRequest) {
 }
 
 func versionHandler(params martini.Params, w http.ResponseWriter, r *http.Request) {
-	versionData := "{\"name\": \"Objectstore\",\"version\": \"1.2.2-a\",\"Change Log\":\"Changed Insert and Update Method execution. Errors may be occur! Alpha build!\",\"author\": {\"name\": \"Duo Software\",\"url\": \"http://www.duosoftware.com/\"},\"repository\": {\"type\": \"git\",\"url\": \"https://github.com/DuoSoftware/v6engine/\"}}"
+	versionData := "{\"name\": \"Objectstore\",\"version\": \"1.2.3-a\",\"Change Log\":\"Tested Transactions!\",\"author\": {\"name\": \"Duo Software\",\"url\": \"http://www.duosoftware.com/\"},\"repository\": {\"type\": \"git\",\"url\": \"https://github.com/DuoSoftware/v6engine/\"}}"
 	fmt.Fprintf(w, versionData)
 }
 
