d22e6a54e6270f984c6cc2c80c607e2fa68efbb4
Prasad Jayashanka <prasadjayashanka@gmail.com>
2017-01-11 11:50:49 +0530

    Added Request Tester

---
+++ RequestTester/RequestTester.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"duov6.com/RequestTester/endpoints"
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	Splash()
+
+	httpServer := endpoints.HTTPService{}
+	httpServer.Start()
+}
+
+func Splash() {
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("______             _    _            _             ")
+	fmt.Println("|  _  \\           | |  | |          | |            ")
+	fmt.Println("| | | |_   _  ___ | |  | | ___  _ __| | _____ _ __ ")
+	fmt.Println("| | | | | | |/ _ \\| |/\\| |/ _ \\| '__| |/ / _ \\ '__|")
+	fmt.Println("| |/ /| |_| | (_) \\  /\\  / (_) | |  |   <  __/ |   ")
+	fmt.Println("|___/  \\__,_|\\___/ \\/  \\/ \\___/|_|  |_|\\_\\___|_|   ")
+	fmt.Println()
+	fmt.Println()
+}
