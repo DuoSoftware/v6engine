501702bdc0be8948d92e64619fade696966c70b3
Prasad Jayashanka <prasadjayashanka@gmail.com>
2017-01-31 13:18:02 +0530

    added file handler pool to test

--- LogTester/endpoints/HTTPService.go
+++ LogTester/endpoints/HTTPService.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/exec"
 	"runtime"
 	"sync"
 	"time"
@@ -67,12 +66,10 @@ func handleRequest(params martini.Params, w http.ResponseWriter, r *http.Request
 func PrintLogs(input map[string]interface{}) {
 	fileName := input["InSessionID"].(string) + ".txt"
 	Body := "This is A Sample Body"
-
 	for x := 0; x < 30; x++ {
 		PublishLog(fileName, Body)
 		//time.Sleep(200 * time.Millisecond)
 	}
-
 }
 
 var fileHandlers map[string]*os.File
@@ -109,31 +106,28 @@ func PublishLog(fileName string, Body string) {
 		os.Mkdir(logrootpath, 0777)
 	}
 
-	if runtime.GOOS == "linux" {
-
-		date := string(time.Now().Local().Format("2006-01-02 @ 15:04:05"))
-		_, _ = exec.Command("sh", "-c", "echo "+date+"    "+Body+" >> "+logfolderName+slash+fileName).Output()
-		//_, _ = exec.Command("sh", "-c", "echo "+Body+" >> "+fileName).Output()
-
+	var ff *os.File
+	var err error
+	name := logfolderName + slash + fileName
+	if GetFileHandler(name) != nil {
+		ff = GetFileHandler(name)
 	} else {
-
-		var ff *os.File
-		var err error
-		name := logfolderName + slash + fileName
-		if GetFileHandler(name) != nil {
-			ff = GetFileHandler(name)
-		} else {
-			ff, err = os.OpenFile(name, os.O_APPEND, 0666)
-			if err != nil {
-				ff, err = os.Create(name)
-				ff, err = os.OpenFile(name, os.O_APPEND, 0666)
-			}
-			SetFileHandler(name, ff)
-		}
-
-		_, err = ff.Write([]byte(string(time.Now().Local().Format("2006-01-02 @ 15:04:05")) + "  " + Body + "  " + "\r\n"))
+		ff, err = os.OpenFile(name, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 		if err != nil {
-			fmt.Println(err.Error() + "asdf")
+			ff, err = os.Create(name)
+			if err == nil {
+				ff, err = os.OpenFile(name, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+				if err == nil {
+					SetFileHandler(name, ff)
+				}
+			}
 		}
+
 	}
+
+	_, err = ff.Write([]byte(string(time.Now().Local().Format("2006-01-02 @ 15:04:05")) + "  " + Body + "  " + "\r\n"))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
