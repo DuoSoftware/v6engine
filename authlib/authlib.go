8c15d85deb9b45dfefd6dd4f754c407f5664c26a
Prasad Jayashanka <prasadjayashanka@gmail.com>
2017-03-24 11:08:50 +0530

    DuoAuth 6.1.23

--- authlib/authlib.go
+++ authlib/authlib.go
@@ -1073,7 +1073,7 @@ func (A Auth) Verify() (output string) {
 
 	versionData := make(map[string]interface{})
 	versionData["API Name"] = "Duo Auth"
-	versionData["API Version"] = "6.1.23b"
+	versionData["API Version"] = "6.1.23"
 
 	changeLogs := make(map[string]interface{})
 
