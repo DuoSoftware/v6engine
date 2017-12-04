8f2439a2a98c43f45716dc900330001f8d351f8b
duobuilduser <duobuilduser@duosoftware.com>
2017-12-04 15:36:59 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -4,6 +4,8 @@ cd /home/DUO_V6ENGINE
 if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]; then
     cp scripts/staging_agent.config /home/DUO_V6_OBSTORE/agent.config
     cp scripts/staging_agent.config /home/DUO_V6_AUTH/agent.config
+    cp scripts/Terminal.config /home/DUO_V6_AUTH/Terminal.config
+    cp scripts/settings.config /home/DUO_V6_AUTH/settings.config
     cp ObjectStore /home/DUO_V6_OBSTORE/
     cp duoauth/duoauth /home/DUO_V6_AUTH
     
