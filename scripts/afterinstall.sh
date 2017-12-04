0b48e8b5f1214b71227adac087bd5aa0e7de6f7b
duobuilduser <duobuilduser@duosoftware.com>
2017-12-04 15:33:50 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -3,7 +3,11 @@
 cd /home/DUO_V6ENGINE
 if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]; then
     cp scripts/staging_agent.config /home/DUO_V6_OBSTORE/agent.config
+    cp scripts/staging_agent.config /home/DUO_V6_AUTH/agent.config
     cp ObjectStore /home/DUO_V6_OBSTORE/
+    cp duoauth/duoauth /home/DUO_V6_AUTH
+    
+    
 elif ["$DEPLOYMENT_GROUP_NAME" == "production" ]; then
    cp scripts/production_agent.config /home/DUO_V6_OBSTORE/agent.config
 fi
