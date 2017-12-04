cef363fc6450b449544586cb21e7ed1f77b8c17b
duobuilduser <duobuilduser@duosoftware.com>
2017-12-04 23:37:12 +0530

    Update applicationstop.sh

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-service duonotifier start
-service duoobjectstore stop
-service duoauth stop
+
+isExistAuth=$(docker ps -a | grep duo-auth)
+isExistObstore=$(docker ps -a | grep duo-obstore)
+isExistNotifier=$(docker ps -a | grep duo-notifier)
+
+if [ $isExistAuth ];
+then
+    docker rm -f duo-auth
+    docker rmi duo-auth
+elif [ isExistObstore ];
+then
+  docker rm -f duo-obstore
+  docker rmi duo-obstore
+
+elif [ isExistNotifier ];
+then
+  docker rm -f duo-notifier
+  docker rmi duo-notifier
+fi
 
