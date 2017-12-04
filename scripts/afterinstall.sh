da9e7812d1c02b5518e77171149797f5c8a3ef4e
duobuilduser <duobuilduser@duosoftware.com>
2017-12-04 18:20:26 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -15,5 +15,15 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]; then
     cp DuoNotifier /home/DUO_V6_NOTIFIER/
         
 elif ["$DEPLOYMENT_GROUP_NAME" == "production" ]; then
-   cp scripts/production_agent.config /home/DUO_V6_OBSTORE/agent.config
+    cp scripts/production_agent.config /home/DUO_V6_OBSTORE/agent.config
+    cp scripts/production_agent.config /home/DUO_V6_AUTH/agent.config
+    cp scripts/production_agent.config /home/DUO_V6_NOTIFIER/agent.config
+     
+    cp scripts/Terminal.config /home/DUO_V6_AUTH/Terminal.config
+    cp scripts/settings.config /home/DUO_V6_AUTH/settings.config
+    cp scripts/settings.config /home/DUO_V6_NOTIFIER/settings.config
+    
+    cp ObjectStore /home/DUO_V6_OBSTORE/
+    cp duoauth/duoauth /home/DUO_V6_AUTH/
+    cp DuoNotifier /home/DUO_V6_NOTIFIER/
 fi
