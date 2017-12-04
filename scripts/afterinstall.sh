f55fdd479f8cb04e99743e492c9a1b256ffe984a
duobuilduser <duobuilduser@duosoftware.com>
2017-12-04 23:33:11 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -9,6 +9,9 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]; then
     cp scripts/Terminal.config /home/DUO_V6_AUTH/Terminal.config
     cp scripts/settings.config /home/DUO_V6_AUTH/settings.config
     cp scripts/settings.config /home/DUO_V6_NOTIFIER/settings.config
+    cp scripts/auth_dockerfile /home/DUO_V6_AUTH/Dockerfile
+    cp scripts/notifier_dockerfile /home/DUO_V6_NOTIFIER/Dockerfile
+    cp scripts/obstore_dockerfile /home/DUO_V6_OBSTORE/Dockerfile
     
     cp ObjectStore /home/DUO_V6_OBSTORE/
     cp duoauth/duoauth /home/DUO_V6_AUTH/
@@ -18,6 +21,10 @@ elif ["$DEPLOYMENT_GROUP_NAME" == "production" ]; then
     cp scripts/production_agent.config /home/DUO_V6_OBSTORE/agent.config
     cp scripts/production_agent.config /home/DUO_V6_AUTH/agent.config
     cp scripts/production_agent.config /home/DUO_V6_NOTIFIER/agent.config
+    
+    cp scripts/auth_dockerfile /home/DUO_V6_AUTH/Dockerfile
+    cp scripts/notifier_dockerfile /home/DUO_V6_NOTIFIER/Dockerfile
+    cp scripts/obstore_dockerfile /home/DUO_V6_OBSTORE/Dockerfile
      
     cp scripts/Terminal.config /home/DUO_V6_AUTH/Terminal.config
     cp scripts/settings.config /home/DUO_V6_AUTH/settings.config
