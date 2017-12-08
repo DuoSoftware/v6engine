5d00cc815ef639ea45a82e1633a4841c05943dc9
Prasad Jayashanka <prasadjayashanka@gmail.com>
2017-12-08 09:47:07 +0530

    6.1.12b

--- objectstore/endpoints/version.go
+++ objectstore/endpoints/version.go
@@ -16,7 +16,7 @@ func GetVersion() string {
 
 	versionData := make(map[string]interface{})
 	versionData["API Name"] = "ObjectStore"
-	versionData["API Version"] = "6.1.12a"
+	versionData["API Version"] = "6.1.12b"
 
 	changeLogs := make(map[string]interface{})
 
--- objectstore/repositories/RepositoryExecutor.go
+++ objectstore/repositories/RepositoryExecutor.go
@@ -149,9 +149,10 @@ func RevertCacheable(request *messaging.ObjectRequest) {
 }
 
 func PushSingleMapToCache(request *messaging.ObjectRequest, obj map[string]interface{}) {
+	go cache.ResetSearchResults(request, cache.Data)
+	return //execute nothing under here
 	if CheckCacheable(request) {
 		RevertCacheable(request)
-		return //execute nothing under here
 		if cache.GetIncrValue(request, ("RequestCounter."+request.Controls.Namespace+"."+request.Controls.Class), cache.RequestCounter) < 1000 {
 			go cache.StoreOne(request, obj, cache.Data)
 		}
@@ -159,6 +160,8 @@ func PushSingleMapToCache(request *messaging.ObjectRequest, obj map[string]inter
 }
 
 func PushMultipleMapToCache(request *messaging.ObjectRequest, objs []map[string]interface{}) {
+	go cache.ResetSearchResults(request, cache.Data)
+	return //execute nothing under here
 	if len(objs) == 1 {
 		return //execute nothing under here
 		if CheckCacheable(request) {
@@ -167,7 +170,5 @@ func PushMultipleMapToCache(request *messaging.ObjectRequest, objs []map[string]
 				go cache.StoreMany(request, objs, cache.Data)
 			}
 		}
-	} else {
-		go cache.ResetSearchResults(request, cache.Data)
 	}
 }
