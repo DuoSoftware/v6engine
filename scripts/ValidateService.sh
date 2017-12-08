5d7e84af2327ee361594795cc31fe881f4a361c1
duobuilduser <duobuilduser@duosoftware.com>
2017-12-08 16:17:57 +0530

    Update ValidateService.sh

--- scripts/ValidateService.sh
+++ scripts/ValidateService.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+function checkport {
+        if nc -zv -w30 $1 $2 <<< '' &> /dev/null
+        then
+                echo "[+] Port $1/$2 is open"
+        else
+                echo "[-] Port $1/$2 is closed"
+                if [ $2 == 3000 ]; then
+                        docker restart duo-obstore
+                elif [ $2 == 3048 ]; then
+                        docker restart duo-auth
+                elif [ $2 == 7000 ]; then
+                        docker restart duo-notifier
+                elif [ $2 == 5000 ]; then
+                        docker restart duo-ceb
+                fi
+        fi
+}
+
+checkport '127.0.0.1' 3000
+checkport '127.0.0.1' 3048
+checkport '127.0.0.1' 7000
+checkport '127.0.0.1' 5000
 
