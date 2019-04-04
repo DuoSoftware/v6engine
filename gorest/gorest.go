d525c96e2d51feac72bb8cbd03fabc0891cd60fd
Shehan Tis <shehanproductions@ymail.com>
2019-04-04 16:28:03 +0530

    added header to accept

--- gorest/gorest.go
+++ gorest/gorest.go
@@ -191,7 +191,7 @@ func (_ manager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		//w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SecurityToken")
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SecurityToken, Tenant, studio.crowd.tokenkey, JSESSIONID, atlassian.xsrf.token, cloud.session.token, Invitetype, Nonce, companyInfo")
 	}
 	// Stop here if its Preflighted OPTIONS request
 	if r.Method == "OPTIONS" {
